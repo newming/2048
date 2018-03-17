8c8c802e54efcd3f5470f261528f39e4d84bce9d
newming <newming1993@gmail.com>
2018-03-17 16:33:44 +0800

    4-5 end

--- main.js
+++ main.js
@@ -75,14 +75,30 @@ function generateOneNumber() {
   // 随机一个位置
   var randx = parseInt(Math.floor(Math.random() * 4))
   var randy = parseInt(Math.floor(Math.random() * 4))
-  while (true) {
+
+  var times = 0 // 随机生成位子尝试的次数，大于50次后，人工选取一个
+  while (times < 50) {
     if (board[randx][randy] === 0) {
       break
     }
 
     var randx = parseInt(Math.floor(Math.random() * 4))
     var randy = parseInt(Math.floor(Math.random() * 4))
+
+    times ++
   }
+
+  if (times === 50) {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (board[i][j] === 0) {
+          randx = i
+          randy = j
+        }
+      }
+    }
+  }
+
   // 随机一个数字
   var randNumber = Math.random() < 0.5 ? 2 : 4
 
